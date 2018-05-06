933db8b9b1ee344849088e07cedc18fd941f913a
Thomas van Dam <t.thomas.van.dam@gmail.com>
2018-05-06 19:19:11 +0200

    Move to virtual MIDI ports so MIDI patching apps can do the routing

--- index.js
+++ index.js
@@ -3,42 +3,45 @@ import nodeCleanUp from 'node-cleanup';
 
 import Sequencer from './lib/Sequencer';
 import LaunchPadMini from './lib/LaunchPadMini';
-import TempoView from './modes/tempo';
-import MainControls from './MainControls';
+import TempoView from './modes/TempoView';
+import SideControls from './SideControls';
 
-MidiStream.getPortNames((err, names) => {
-    console.log('names ::: ', names);
+const lpStream = MidiStream('Cirquencer:controls', { virtual: true });
 
-    const [lpName] = names;
-    const lpStream = MidiStream(lpName);
+console.log('MIDI plumbing in place? [y/n]');
 
-    const lpController = new LaunchPadMini(lpStream);
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', (input) => {
+    if (input === 'y\n') {
+        const lpController = new LaunchPadMini(lpStream);
 
-    const activeView = new TempoView(lpController);
-    const controls = new MainControls(lpController);
-    activeView.load();
-    controls.drawButtons();
+        const activeView = new TempoView(lpController);
+        const controls = new SideControls(lpController);
+        activeView.load();
+        controls.drawButtons();
 
-    Sequencer.eventStream.on('data', (data) => {
-        if (data.id === 'test') {
-            const [key] = data.args;
+        Sequencer.eventStream.on('data', (data) => {
+            if (data.id === 'test') {
+                const [key] = data.args;
 
-            if (data.event === 'start') {
-                lpController.output.write({
-                    key,
-                    value: 127,
-                });
-            } else {
-                lpController.output.write({
-                    key,
-                    value: 0,
-                });
+                if (data.event === 'start') {
+                    lpController.output.write({
+                        key,
+                        value: 127,
+                    });
+                } else {
+                    lpController.output.write({
+                        key,
+                        value: 0,
+                    });
+                }
             }
-        }
-    });
+        });
 
-    nodeCleanUp(() => {
-        lpController.reset();
-        return true;
-    });
+        nodeCleanUp(() => {
+            lpController.reset();
+            return true;
+        });
+    }
 });
